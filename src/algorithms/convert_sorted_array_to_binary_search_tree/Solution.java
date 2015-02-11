360970dca845092c3b3b1608d1b6456487f88671
Qiao Zhang <qiao.zhang.2015@gmail.com>
2015-02-11 21:31:32 +1100

    finished 108 - Convert Sorted Array to BST (balanced)

---
+++ src/algorithms/convert_sorted_array_to_binary_search_tree/Solution.java
@@ -0,0 +1,23 @@
+/*
+ * Solution to 108 - Convert Sorted Array to Binary Search Tree
+ */
+package algorithms.convert_sorted_array_to_binary_search_tree;
+
+import java.util.Arrays;
+
+public class Solution {
+	public TreeNode sortedArrayToBST(int[] arr) {
+		if (arr.length == 0) {
+			return null;
+		}
+
+		int medianVal = arr[arr.length / 2];
+		TreeNode root = new TreeNode(medianVal);
+		int[] leftHalf = Arrays.copyOfRange(arr, 0, arr.length / 2);
+		int[] rightHalf = Arrays.copyOfRange(arr, arr.length / 2 +1, arr.length);
+		root.left = sortedArrayToBST(leftHalf);
+		root.right = sortedArrayToBST(rightHalf);
+		
+		return root;
+	}
+}
---
+++ src/algorithms/convert_sorted_array_to_binary_search_tree/TreeNode.java
@@ -0,0 +1,12 @@
+package algorithms.convert_sorted_array_to_binary_search_tree;
+
+/*
+ * This class is provided by LeetCode.
+ */
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+}
