4af493314a7ac5435e091e84352ab3d7c1950207
Qiao Zhang <qiao.zhang.2015@gmail.com>
2015-02-11 21:14:03 +1100

    finished 21 - Merge Two Sorted List

---
+++ src/algorithms/merge_two_sorted_lists/ListNode.java
@@ -0,0 +1,13 @@
+package algorithms.merge_two_sorted_lists;
+
+/**
+ * This class is provided by LeetCode.
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
---
+++ src/algorithms/merge_two_sorted_lists/Solution.java
@@ -0,0 +1,44 @@
+/*
+ * Solution for 21 - Merge Two Sorted Lists
+ * https://oj.leetcode.com/problems/merge-two-sorted-lists/
+ */
+
+package algorithms.merge_two_sorted_lists;
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        
+        ListNode start;
+        ListNode end;
+        if (l1.val <= l2.val) {
+            start = l1;
+            l1 = l1.next;
+        } else {
+            start = l2;
+            l2 = l2.next;
+        }
+        end = start;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                end.next = l1;
+                end = end.next;
+                l1 = l1.next;
+            } else {
+                end.next = l2;
+                end = end.next;
+                l2 = l2.next;
+            }
+        }
+        
+        if (l1 == null) {
+            end.next = l2;
+        } else {
+            end.next = l1;
+        }
+        
+        return start;
+    }
+}
