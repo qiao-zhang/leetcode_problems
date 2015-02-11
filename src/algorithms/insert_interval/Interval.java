fd5168e343c93912495825f4b06b0cbc507775b1
Qiao Zhang <qiao.zhang.2015@gmail.com>
2015-02-11 20:58:55 +1100

    minor change for 57 - insert interval

---
+++ src/algorithms/insert_interval/Interval.java
@@ -0,0 +1,11 @@
+package algorithms.insert_interval;
+
+/**
+ * This class is provided by LeetCode.
+ */
+public class Interval {
+	int start;
+	int end;
+	Interval() { start = 0; end = 0; }
+	Interval(int s, int e) { start = s; end = e; }
+}
