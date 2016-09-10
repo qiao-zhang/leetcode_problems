d4f52eeb4cd92af3900932a898ce4e7a45e25c03
Qiao Zhang <qiaozhang@Qiaos-MBP.frontier.com>
2016-09-10 11:07:20 -0400

    update 007

--- algorithms/_007_reverse_integer.py
+++ algorithms/_007_reverse_integer.py
@@ -9,5 +9,7 @@ class Solution(object):
         m, s, r = abs(x), x / abs(x), 0
         while m > 0:
             r = r * 10 + m % 10
+            if (x < 0 and r > 2**31) or (x > 0 and r >= 2**31) :
+                return 0
             m = m // 10
         return r * s
