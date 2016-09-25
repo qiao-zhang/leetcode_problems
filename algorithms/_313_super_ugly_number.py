8f8b09207d8c10a86e3f6a407baac57b1f7f5681
Qiao Zhang <qiao.zhang.2015@gmail.com>
2016-09-25 13:14:51 -0400

    update 313

--- algorithms/_313_super_ugly_number.py
+++ algorithms/_313_super_ugly_number.py
@@ -5,9 +5,24 @@ class Solution(object):
         :type primes: List[int]
         :rtype: int
         """
-        uglies = [1]
-        merged = heapq.merge(*map(lambda p: (u * p for u in uglies), primes))
-        uniqued = (u for u, _ in itertools.groupby(merged))
-        while len(uglies) < n:
-            uglies.append(next(uniqued))
-        return uglies[-1]
+        def solveRegularly():
+            assert n > 0
+            uglies, ugliesSet, hp = [1], {1}, [(p, p, 0) for p in primes]
+            while len(uglies) < n:
+                u, p, i = heapq.heappop(hp)
+                if u not in ugliesSet:
+                    uglies.append(u)
+                    ugliesSet.add(u)
+                i += 1
+                heapq.heappush(hp, (p * uglies[i], p, i))
+            return uglies[-1]
+
+        def solveUsingGenerator():
+            assert n > 0
+            uglies = [1]
+            merged = heaqp.merge(*map(lambda p: (p * u for u in uglies), primes))
+            uniqued = (u for u, _ in itertools.groupby(merged))
+            while len(uglies) < n:
+                uglies.append(next(uniqued))
+            return uglies[-1]
+        return solveRegularly()
